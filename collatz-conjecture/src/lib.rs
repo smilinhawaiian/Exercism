3ba4d9c5d1ba34587a09ddc11903d4251646cdf6
smilinhawaiian <shepherd.sharice@gmail.com>
2019-07-22 11:52:30 -0700

    completed rough sol for collatz

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,18 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    //unimplemented!(
+    //    "return Some(x) where x is the number of steps required to reach 1 starting with {}",
+    //    n,
+    //)
+    let mut count: u64 = 0;
+    if n < 1 {
+        return None
+    }
+    else if n > 1 {
+        if n % 2 == 0 { // even
+            count = collatz(n/2).unwrap() + 1;
+        } else { // odd
+            count = collatz(3*n +1).unwrap() + 1;
+        }
+    }
+    Some(count)
 }
